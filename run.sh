16fc5b97218386eed8806c5816b2aea8c6069581
Merthan Karaman <79408454+merth4n@users.noreply.github.com>
2022-07-26 16:29:11 +0300

    26.07.22

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 npm install
 sudo mv cncjs-bedlevel.service /etc/systemd/system/cncjs-bedlevel.service 
+sudo chmod 775 -R ~/cncjs-bedlevel/
+sudo systemctl start cncjs-bedlevel.service 
+sudo systemctl enable cncjs-bedlevel.service 
